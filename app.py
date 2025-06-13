3f2b15eca486a896162dd46ceb9e7d2711d67f45
dpkaru <dpkaru10@gmail.com>
2025-06-13 19:59:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import tensorflow as tf
 import numpy as np
 from PIL import Image
 
+@st.cache_resource
+def load_model():
+    return tf.keras.models.load_model("model.h5")
+
+model = load_model()
+
 st.title("Anomaly Detection using Teachable Machine Model")
 
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
@@ -14,7 +20,6 @@ if uploaded_file is not None:
     img = img.resize((224, 224))
     img_array = tf.keras.utils.img_to_array(img)
     img_array = tf.expand_dims(img_array, 0)
-    model = tf.keras.models.load_model("converted_model")
     
     predictions = model.predict(img_array)
     class_names = ["Normal", "Defected"]
